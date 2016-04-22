41fe12ef3870adc521690276f8f6a63c55400507
lylgood <liyalu@liyaludeMacBook-Pro.local>
2016-04-23 00:27:35 +0800

    add CardActivity

---
+++ DemoApplication/app/src/main/java/com/example/liyalu/myapplication/test/CardActivity.java
@@ -0,0 +1,18 @@
+package com.example.liyalu.myapplication.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.liyalu.myapplication.R;
+
+/**
+ * Created by liyalu on 16/4/22.
+ */
+public class CardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+    }
+}
