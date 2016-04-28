2be7b3237c22174eaec2195f1fd280e356949ac6
lylgood <liyalu@liyaludeMacBook-Pro.local>
2016-04-28 14:11:09 +0800

    add files

---
+++ DemoApplication/app/src/main/java/com/example/liyalu/myapplication/activity/DetailsPageActivity.java
@@ -0,0 +1,19 @@
+package com.example.liyalu.myapplication.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.liyalu.myapplication.R;
+
+/**
+ * Created by liyalu on 16/4/28.
+ */
+public class DetailsPageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_page);
+
+    }
+}
